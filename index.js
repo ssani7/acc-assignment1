268d0a7519b73b81ee7d34e841aba030c0c10d0a
ssani7 <sanaullah.sani756@gmail.com>
2022-09-03 22:59:08 +0600

    heroku port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 const fs = require('fs');
 const path = require('path');
 const userRoute = require('./routes/v1/user.routes');
@@ -24,6 +25,6 @@ const run = async () => {
 }
 run().catch(console.dir);
 
-app.listen(5000, () => {
-    console.log("server running at 5000");
+app.listen(port, () => {
+    console.log(`server running at ${port}`);
 })
